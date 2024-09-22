fa94e2afb0e71ef60da48d1c9ca5345ceb2fc49d
Rummonislam786 <islam.rummon786@gmail.com>
2024-09-23 02:46:23 +0600

    aboutScreen added

--- lib/FaceDetectionScreen.dart
+++ lib/FaceDetectionScreen.dart
@@ -1,3 +1,4 @@
+import 'package:Attendance_System/dashboard.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -12,11 +13,50 @@ class FaceDetectionScreen extends StatefulWidget {
 class _FaceDetectionScreenState extends State<FaceDetectionScreen> {
   CameraController? _controller;
   Future<void>? _initializeControllerFuture;
+  List<CameraDescription> _availableCameras = [];
+  Future<void> _initCamera(CameraDescription description) async {
+    _controller =
+        CameraController(description, ResolutionPreset.max, enableAudio: true);
+
+    try {
+      await _controller!.initialize();
+      // to notify the widgets that camera has been initialized and now camera preview can be done
+      setState(() {});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _toggleCameraLens() {
+    // get current lens direction (front / rear)
+    final lensDirection = _controller!.description.lensDirection;
+    CameraDescription newDescription;
+    if (lensDirection == CameraLensDirection.front) {
+      newDescription = _availableCameras.firstWhere((description) =>
+          description.lensDirection == CameraLensDirection.back);
+    } else {
+      newDescription = _availableCameras.firstWhere((description) =>
+          description.lensDirection == CameraLensDirection.front);
+    }
+
+    if (newDescription != null) {
+      _initCamera(newDescription);
+    } else {
+      print('Asked camera not available');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     _initializeCamera();
+    _getAvailableCameras();
+  }
+
+  Future<void> _getAvailableCameras() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    _availableCameras = await availableCameras();
+    _initCamera(_availableCameras.first);
   }
 
   Future<void> _initializeCamera() async {
@@ -62,22 +102,46 @@ class _FaceDetectionScreenState extends State<FaceDetectionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Face Detection')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller!);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _captureAndDetect,
-        child: Icon(Icons.camera_alt),
-      ),
-    );
+        appBar: AppBar(title: Text('Face Detection')),
+        body: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(_controller!);
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          onPressed: _captureAndDetect,
+          child: Icon(Icons.camera_alt),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: const Color.fromRGBO(82, 170, 94, 1.0),
+          shape: const CircularNotchedRectangle(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Dashboard()));
+                  },
+                  icon: const Icon(Icons.home,
+                      size: 40.0, color: Color.fromRGBO(43, 217, 254, 1.0))),
+              IconButton(
+                  onPressed: _toggleCameraLens,
+                  icon: const Icon(Icons.camera_rear,
+                      size: 40.0, color: Colors.red))
+            ],
+          ),
+        ));
   }
 
   @override
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,5 @@
+import 'package:Attendance_System/FaceDetectionScreen.dart';
+import 'package:Attendance_System/about.dart';
 import 'package:flutter/material.dart';
 import 'package:Attendance_System/Models/users.dart';
 import 'package:Attendance_System/SqliteFunc/sqlite.dart';
@@ -13,11 +15,23 @@ class _DashboardState extends State<Dashboard> {
   late DatabaseHelper handler;
   late Future<List<Users>> users;
 
-  final List<Map<String, dynamic>> menuItems = [
-    {"title": "Main", "icon": Icons.psychology, "color": Colors.blue},
-    {"title": "Users", "icon": Icons.person, "color": Colors.green},
-    {"title": "Configuration", "icon": Icons.settings, "color": Colors.orange},
-    {"title": "About", "icon": Icons.info, "color": Colors.purple},
+  var menuItems = [
+    // {"title": "Main", "icon": Icons.psychology, "color": Colors.blue},
+    // {"title": "Users", "icon": Icons.person, "color": Colors.green},
+    // {"title": "Configuration", "icon": Icons.settings, "color": Colors.orange},
+    // {"title": "About", "icon": Icons.info, "color": Colors.purple},
+
+    // GridView.builder(
+    //         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    //           crossAxisCount: 2,
+    //           crossAxisSpacing: 16.0,
+    //           mainAxisSpacing: 16.0,
+    //           childAspectRatio: 1.1,
+    //         ),
+    //         itemCount: menuItems.length,
+    //         itemBuilder: (context, index) {
+    //           return _buildMenuItem(menuItems[index]);
+    //         },
   ];
 
   @override
@@ -46,18 +60,52 @@ class _DashboardState extends State<Dashboard> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          child: GridView.count(
+              crossAxisSpacing: 1,
+              mainAxisSpacing: 2,
               crossAxisCount: 2,
-              crossAxisSpacing: 16.0,
-              mainAxisSpacing: 16.0,
-              childAspectRatio: 1.1,
-            ),
-            itemCount: menuItems.length,
-            itemBuilder: (context, index) {
-              return _buildMenuItem(menuItems[index]);
-            },
-          ),
+              children: [
+                InkWell(
+                  child: _buildMenuItem({
+                    "title": "Detect Emotion",
+                    "icon": Icons.camera,
+                    "color": Colors.blue
+                  }),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => FaceDetectionScreen()));
+                  },
+                ),
+                InkWell(
+                  child: _buildMenuItem({
+                    "title": "Users",
+                    "icon": Icons.person,
+                    "color": Colors.green
+                  }),
+                  onTap: () {},
+                ),
+                InkWell(
+                  child: _buildMenuItem({
+                    "title": "Configuration",
+                    "icon": Icons.settings,
+                    "color": Colors.orange
+                  }),
+                  onTap: () {},
+                ),
+                InkWell(
+                  child: _buildMenuItem({
+                    "title": "About",
+                    "icon": Icons.info,
+                    "color": Colors.purple
+                  }),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AboutScreen()));
+                  },
+                ),
+              ]),
         ),
       ),
     );
@@ -67,38 +115,33 @@ class _DashboardState extends State<Dashboard> {
     return Card(
       elevation: 4,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      child: InkWell(
-        onTap: () {
-          // Handle menu item tap
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [item['color'], item['color'].withOpacity(0.7)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [item['color'], item['color'].withOpacity(0.7)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              item['icon'],
+              size: 48,
+              color: Colors.white,
             ),
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                item['icon'],
-                size: 48,
+            const SizedBox(height: 12),
+            Text(
+              item['title'],
+              style: const TextStyle(
                 color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 12),
-              Text(
-                item['title'],
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
